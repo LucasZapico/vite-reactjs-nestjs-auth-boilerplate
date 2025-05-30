3fec468d5b44b87b4cbc3276af762af12e82dffb
Lucas Zapico <hello@lucaszapico.space>
2025-05-30 07:23:42 -0700

    feat(providers): add provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '@/styles/index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router'
-import { RouteWrapper } from '@/routes.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import { RouteWrapper } from "@/routes.tsx";
+import { Providers } from "./providers";
+import "@/styles/index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <RouteWrapper/>
+      <Providers>
+        <RouteWrapper />
+      </Providers>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
