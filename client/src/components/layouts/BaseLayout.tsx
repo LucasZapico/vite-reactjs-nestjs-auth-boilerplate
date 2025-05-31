6d15bdbeb1e9da33b0a4a45ee535a5d433b84a52
Lucas Zapico <hello@lucaszapico.space>
2025-05-31 08:47:12 -0700

    feat(auth): add user login and redirects

--- client/src/components/layouts/BaseLayout.tsx
+++ client/src/components/layouts/BaseLayout.tsx
@@ -8,7 +8,8 @@ import { logError, logInfo } from "@/utils/logger";
 import Cookies from "js-cookie";
 import { useNavigate } from "react-router";
 import { useMutation } from "@tanstack/react-query";
-import { PrimeApi } from "@/config/axios.config";
+import { addBearerToken, PrimeApi } from "@/config/axios.config";
+import { handleCookeis } from "@/config/features.config";
 
 export const BaseLayout = ({ children }: { children?: React.ReactNode }) => {
   const [mode, setMode] = useAtom(modeAtom);
@@ -20,6 +21,9 @@ export const BaseLayout = ({ children }: { children?: React.ReactNode }) => {
         method: "post",
         url: "/auth/logout",
         data,
+        headers: {
+          Authorization: `Bearer ${data.accessToken}`,
+        },
       });
     },
     onError: (error: any) => {
@@ -28,23 +32,43 @@ export const BaseLayout = ({ children }: { children?: React.ReactNode }) => {
     onSuccess: (data: any) => {
       logInfo("Logout success", data);
 
-      Cookie.set("blue_monkey_user", JSON.stringify(null));
+      // Cookies.remove("blue_monkey_user");
       setUser(null);
       navigate("/");
     },
   });
 
   useEffect(() => {
-    const cookieUser = Cookies.get("blue_monkey_user");
-    if (!cookieUser) {
-      logInfo("No user cookie found, redirecting to login");
-      navigate("/auth/login");
-      return;
+    if (handleCookeis) {
+      const cookieUser = Cookies.get("blue_monkey_user");
+
+      if (!cookieUser) {
+        logInfo("No user cookie found, redirecting to login");
+        navigate("/auth/login");
+        return;
+      }
+      const parsedUser = JSON.parse(cookieUser).data;
+      logInfo("User cookie found", parsedUser);
+      const addBearer = addBearerToken(PrimeApi, parsedUser.accessToken);
+      if (addBearer) {
+
+        setUser({
+          ...parsedUser.user,
+          accessToken: parsedUser.accessToken,
+          refreshToken: parsedUser.refreshToken,
+        });
+        navigate("/home");
+      }
     }
-    setUser(JSON.parse(cookieUser));
   }, []);
 
-  useEffect(() => {}, [user]);
+  useEffect(() => {
+    if(!user) {
+      logInfo("No user found, redirecting to login");
+      navigate("/auth/login");
+      return;
+    } 
+  }, [user]);
 
   return (
     <div className="flex flex-col min-h-screen">
@@ -63,7 +87,15 @@ export const BaseLayout = ({ children }: { children?: React.ReactNode }) => {
               </div>
             ) : (
               <>
-                <Button variant="secondary">Log Out</Button>
+                <Button
+                  variant="secondary"
+                  onClick={() => {
+                    mutation.reset();
+                    mutation.mutate(user);
+                  }}
+                >
+                  Log Out
+                </Button>
               </>
             )}
 
